549ffb2db32521446cdd3dfcdcd1f12a6eeaa3c5
Julian Santoyo <113941820+juliansant0y0@users.noreply.github.com>
2026-06-12 08:46:59 -0500

    Implement identity operator examples in identity_operators.py
    Add identity operator examples demonstrating 'is' and 'is not' usage.

---
+++ identity_operators.py
@@ -0,0 +1,24 @@
+a = 3
+b = 3  
+c = 4
+print (a is b) 
+print (a is not b) 
+print (a is not c) 
+
+x = 1
+y = x
+z = y
+       
+print (z is 1) 
+print (z is x)
+
+str1 = "FreeCodeCamp"
+str2 = "FreeCodeCamp
+
+print (str1 is str2) 
+print ("Code" is str2) 
+
+a = [10,20,30]
+b = [10,20,30
+
+print (a is b) #  (las listas son objetos mutables en Python)  
