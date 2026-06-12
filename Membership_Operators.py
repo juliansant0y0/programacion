13bc3d6f986628fbd193dd121e27aab282781c80
Julian Santoyo <113941820+juliansant0y0@users.noreply.github.com>
2026-06-12 08:45:29 -0500

    Add Membership Operators examples in Python

---
+++ Membership_Operators.py
@@ -0,0 +1,19 @@
+#Operadores de Pertenencia
+
+a = [1,2,3,4,5]    # lista
+  
+#Esta 3 en la lista a?
+print (3 in a) 
+  
+#No está 12 en la lista a?
+print (12 not in a) 
+  
+str = "Hello World"
+  
+#Contiene World el string str?
+print ("World" in str) 
+  
+#Contiene world el string str? (nota: distingue mayúsculas y minúsculas)
+print ("world" in str) 
+
+print ("code" not in str) 
