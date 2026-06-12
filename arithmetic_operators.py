7c75a578274435f842128a2546a524d25d478d73
Julian Santoyo <113941820+juliansant0y0@users.noreply.github.com>
2026-06-12 08:40:45 -0500

    Add arithmetic operators demonstration in Python

---
+++ arithmetic_operators.py
@@ -0,0 +1,31 @@
+print ('Operadores aritméticos')
+
+a, b, c, d = 26, 11.3, 5, 3.5
+
+print ('a = ', a, ', b = ', b , ', c = ', c, ', d = ', d )
+
+# Suma, Añade valores a cada lado del operador
+print ('a + b = ', a + b)
+
+# Resta, Resta el operando de la derecha del operador del lado izquierdo
+print ('c - a = ',c - a)
+
+# Multiplicación, Multiplica los valores de ambos lados del operador
+print ('d * c =', d * c)
+
+# Exponente, Realiza el cálculo exponencial (potencia) de los operadores
+print ('c ** 2 =', c ** 2)
+
+# División
+print ('float(c) / a =', float(c) / a)
+
+# División entera
+print (' 7 / 3 =', 7 / 3
+
+# Cociente de una división, La división de operandos que el resultado es el cociente 
+# en el cual se eliminan los dígitos después del punto decimal.
+print ('a // c =', a // c)
+
+# Módulo, Divide el operando de la izquierda por el 
+# operador del lado derecho y devuelve el resto.
+print (7 % 3 =', 7 % 3)
