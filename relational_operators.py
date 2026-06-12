8affbd51561bf492b6672afa2406f03ac0adbf1b
Julian Santoyo <113941820+juliansant0y0@users.noreply.github.com>
2026-06-12 08:42:56 -0500

    Implement examples of relational operators in Python
    Add relational operators examples and comparisons

---
+++ relational_operators.py
@@ -0,0 +1,38 @@
+print ('Operadores relacionales o comparación')
+
+a, b, a1, b1, c1 = 5, 5, 7, 3, 3
+cadena1, cadena2 = 'Hola', 'Adiós'
+lista1, lista2 = [1, 'Lista Python', 23], [11, 'Lista Python', 23]
+
+# Igual - El operador == evalúa que los valores sean iguales para varios tipos de datos.
+c = a == b
+print c)
+
+cadenas = cadena1 == cadena2
+print (cadenas)
+
+listas = lista1 == lista2
+print (listas)
+
+# Diferente - El operador != evalúa si los valores son distintos.
+d = a1 != b
+print (d)
+
+cadena0 = cadena1 != cadena2
+print (cadena0)
+
+# Menor que - El operador < evalúa si el valor del lado izquierdo es menor que el valor del lado derecho.
+f = b1 < a1
+print (f)
+
+# Mayor que - El operador > evalúa si el valor del lado izquierdo es mayor que el valor del lado derecho.
+e = a1 > b1
+print (e)
+
+# Menor o igual que - El operador <= evalúa si el valor del lado izquierdo es menor o igual que el valor del lado derecho.
+h = b1 <= c1
+print (h
+
+# Mayor o igual que - El operador >= evalúa si el valor del lado izquierdo es mayor o igual que el valor del lado derecho.
+g = b1 >= c1
+print (g)
